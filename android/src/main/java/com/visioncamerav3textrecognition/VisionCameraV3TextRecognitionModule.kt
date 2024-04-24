895f8cd47c0d2ab2be844a73ebcc93dbe8ac51ad
Pedro Bueno <pedrolbb.e@gmail.com>
2024-04-24 11:27:57 -0300

    fix(Android): upgrade for react-native-vision v4 API

--- android/src/main/java/com/visioncamerav3textrecognition/VisionCameraV3TextRecognitionModule.kt
+++ android/src/main/java/com/visioncamerav3textrecognition/VisionCameraV3TextRecognitionModule.kt
@@ -14,18 +14,15 @@ import com.google.mlkit.vision.text.devanagari.DevanagariTextRecognizerOptions
 import com.google.mlkit.vision.text.japanese.JapaneseTextRecognizerOptions
 import com.google.mlkit.vision.text.korean.KoreanTextRecognizerOptions
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions
-import com.mrousavy.camera.frameprocessor.Frame
-import com.mrousavy.camera.frameprocessor.FrameProcessorPlugin
-import com.mrousavy.camera.frameprocessor.VisionCameraProxy
-import com.mrousavy.camera.types.Orientation
-
+import com.mrousavy.camera.frameprocessors.Frame
+import com.mrousavy.camera.frameprocessors.FrameProcessorPlugin
+import com.mrousavy.camera.frameprocessors.VisionCameraProxy
 
 class VisionCameraV3TextRecognitionModule(proxy : VisionCameraProxy, options: Map<String, Any>?): FrameProcessorPlugin() {
   override fun callback(frame: Frame, arguments: Map<String, Any>?): Any {
       try {
         val mediaImage: Image = frame.image
-        val orientation : Orientation = frame.orientation
-        val image = InputImage.fromMediaImage(mediaImage, orientation.toDegrees())
+        val image = InputImage.fromMediaImage(mediaImage, frame.imageProxy.imageInfo.rotationDegrees)
         val recognizer : TextRecognizer = when (arguments?.get("language")) {
           "latin" -> TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
           "chinese" -> TextRecognition.getClient(ChineseTextRecognizerOptions.Builder().build())
